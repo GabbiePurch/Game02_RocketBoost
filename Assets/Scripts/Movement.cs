b7019b3e523507df9c0729c576ba2c6fca9f0006
GabbiePurch <purchi375@morrisville.edu>
2025-09-24 14:33:08 -0400

    Basic Game Design Outline Created

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Debug.Log("");
+    }
+
+    void Update()
+    {
+        
+    }
+}
